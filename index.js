f6c6b2160ed7d0ed8df70c20c0a42b98af04089d
Joseph Sutera <joesutera11@gmail.com>
2022-07-15 22:37:54 -0400

    commit tree 532f0c44afc52ac5702ca4ca9775d74c4c542713

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(names,string){
+ 
+   return names.filter(name=>name.toUpperCase()===string.toUpperCase())
+}
+ function fuzzyMatch(driver,match){
+ return driver.filter(driver=>driver.charAt(0)===match.charAt(0))
+ }
+ function matchName(drivers,name){
+    return  drivers.filter(driver=>driver.name===name)
+}
+ 
